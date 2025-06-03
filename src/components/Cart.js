f4d8ac54a59d818c77c674301e4332b41f9636b2
CaioNishijo <110948758+CaioNishijo@users.noreply.github.com>
2025-06-02 21:55:25 -0300

    começo do carro de compras

---
+++ src/components/Cart.js
@@ -0,0 +1,58 @@
+import styled from "styled-components"
+import Button from "./Button"
+
+const Sli = styled.li`
+    margin-bottom: 10px;
+    padding: 16px;
+    border-radius: 8px;
+    background: #f2f2f2;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`
+
+const SDivInfo = styled.div`
+    p{
+        font-size: 16px;
+        margin-bottom: 2px;
+    }
+
+    span{
+        font-size: 16px;
+        font-weight: bold;
+    }
+`
+
+const SDivUnits = styled.div`
+    width: 86px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    button{
+        padding: 5px 10px;
+        border: 1px solid black;
+        border-radius: 5px;
+        cursor: pointer;    
+    }
+`
+
+function CartProduct({ product, onChange, isLoading }){
+    return(
+        <Sli>
+            <SDivInfo>
+                <p>{product.name}</p>
+                <span>R${product.price}</span>
+            </SDivInfo>
+            <SDivUnits>
+                <button disabled={isLoading} onClick={() => onChange(product, -1)}>
+                    -
+                </button>
+                <p>{products.units}</p>
+                <button disabled={isLoading} onClick={() => onChange(product, +1)}>
+                    +
+                </button>
+            </SDivUnits>
+        </Sli>
+    )
+}
